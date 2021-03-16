157b0766673c26b3c86911a4bc201e3ac0eaeb25
swilliamson973 <80009482+swilliamson973@users.noreply.github.com>
2021-03-16 15:43:46 +1300

    commit 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,25 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+import Layout from "../components/layout"
+
 import SEO from '~/components/seo'
 import ProductGrid from '~/components/ProductGrid'
 
-const IndexPage = () => (
-  <>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Shop powered by Gatsby and Shopify.</p>
-    <ProductGrid />
-    <Link to="/page-2/">Go to page 2</Link>
-  </>
-)
+export default function Home() {
+  return <Layout>Hello World!</Layout>
+}
+
+function IndexPage() {
+  return (
+    <>
+      <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Shop powered by Gatsby and Shopify.</p>
+      <ProductGrid />
+      <Link to="/page-2/">Go to page 2</Link>
+    </>
+  )
+}
 
 export default IndexPage
